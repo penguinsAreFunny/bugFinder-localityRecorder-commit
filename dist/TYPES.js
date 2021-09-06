66a6812f91289d2414da6fb2099f5cad1939a46e
Robert Klinger <robert@k1inger.de>
2021-09-06 17:38:19 +0200

    1.0.2

--- dist/TYPES.js
+++ dist/TYPES.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.BUGFINDER_LOCALITYRECORDER_COMMIT_TYPES = void 0;
 exports.BUGFINDER_LOCALITYRECORDER_COMMIT_TYPES = {
-    git: Symbol.for("Git"),
-    gitOptions: Symbol.for("GitOptions"),
-    gitCommitParser: Symbol.for("GitCommitParser"),
-    madFilesFromCommitParser: Symbol.for("madFilesFromCommitParser"),
+    git: Symbol("Git"),
+    gitOptions: Symbol("GitOptions"),
+    gitCommitParser: Symbol("GitCommitParser"),
+    madFilesFromCommitParser: Symbol("madFilesFromCommitParser"),
 };
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiVFlQRVMuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvVFlQRVMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBQWEsUUFBQSx1Q0FBdUMsR0FBRztJQUNuRCxHQUFHLEVBQXlCLE1BQU0sQ0FBQyxHQUFHLENBQUMsS0FBSyxDQUFDO0lBQzdDLFVBQVUsRUFBa0IsTUFBTSxDQUFDLEdBQUcsQ0FBQyxZQUFZLENBQUM7SUFDcEQsZUFBZSxFQUFhLE1BQU0sQ0FBQyxHQUFHLENBQUMsaUJBQWlCLENBQUM7SUFDekQsd0JBQXdCLEVBQUksTUFBTSxDQUFDLEdBQUcsQ0FBQywwQkFBMEIsQ0FBQztDQUNyRSxDQUFBIn0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiVFlQRVMuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvVFlQRVMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBQWEsUUFBQSx1Q0FBdUMsR0FBRztJQUNuRCxHQUFHLEVBQXlCLE1BQU0sQ0FBQyxLQUFLLENBQUM7SUFDekMsVUFBVSxFQUFrQixNQUFNLENBQUMsWUFBWSxDQUFDO0lBQ2hELGVBQWUsRUFBYSxNQUFNLENBQUMsaUJBQWlCLENBQUM7SUFDckQsd0JBQXdCLEVBQUksTUFBTSxDQUFDLDBCQUEwQixDQUFDO0NBQ2pFLENBQUEifQ==
