2eb80a6ec32d5e1b84376b933aefc747e247239b
Robert Klinger <robert@k1inger.de>
2021-09-15 12:04:48 +0200

    1.1.0

--- dist/index.js
+++ dist/index.js
@@ -10,8 +10,17 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+var bugfinder_framework_1 = require("bugfinder-framework");
+var git_1 = require("./git");
+var TYPES_1 = require("./TYPES");
 __exportStar(require("./commit"), exports);
 __exportStar(require("./commitRecorder"), exports);
 __exportStar(require("./git"), exports);
 __exportStar(require("./TYPES"), exports);
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7O0FBQUEsMkNBQXdCO0FBQ3hCLG1EQUFnQztBQUNoQyx3Q0FBcUI7QUFDckIsMENBQXVCIn0=
+/**
+ * Default configuration
+ */
+bugfinder_framework_1.localityAContainer.bind(TYPES_1.BUGFINDER_LOCALITYRECORDER_COMMIT_TYPES.git).to(git_1.GitImpl);
+bugfinder_framework_1.localityAContainer.bind(TYPES_1.BUGFINDER_LOCALITYRECORDER_COMMIT_TYPES.gitCommitParser).to(git_1.FormatParser);
+bugfinder_framework_1.localityAContainer.bind(TYPES_1.BUGFINDER_LOCALITYRECORDER_COMMIT_TYPES.madFilesFromCommitParser).to(git_1.MADFilesFromLogImpl);
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7O0FBQUEsMkRBQXVEO0FBQ3ZELDZCQUEwRjtBQUMxRixpQ0FBZ0U7QUFFaEUsMkNBQXdCO0FBQ3hCLG1EQUFnQztBQUNoQyx3Q0FBcUI7QUFDckIsMENBQXVCO0FBRXZCOztHQUVHO0FBQ0gsd0NBQWtCLENBQUMsSUFBSSxDQUF1QiwrQ0FBdUMsQ0FBQyxHQUFHLENBQUMsQ0FBQyxFQUFFLENBQUMsYUFBTyxDQUFDLENBQUE7QUFDdEcsd0NBQWtCLENBQUMsSUFBSSxDQUF1QiwrQ0FBdUMsQ0FBQyxlQUFlLENBQUMsQ0FBQyxFQUFFLENBQUMsa0JBQVksQ0FBQyxDQUFBO0FBQ3ZILHdDQUFrQixDQUFDLElBQUksQ0FBdUIsK0NBQXVDLENBQUMsd0JBQXdCLENBQUMsQ0FBQyxFQUFFLENBQUMseUJBQW1CLENBQUMsQ0FBQSJ9
