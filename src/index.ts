eb0eb6f985a28ea906ca4ed8e5aa96b90b29eb76
Robert Klinger <robert@k1inger.de>
2021-09-15 12:04:32 +0200

    feat/refactor: added default configuration

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
+import {localityAContainer} from "bugfinder-framework";
+import {FormatParser, Git, GitImpl, MADFilesFromCommit, MADFilesFromLogImpl} from "./git";
+import {BUGFINDER_LOCALITYRECORDER_COMMIT_TYPES} from "./TYPES";
+
 export * from "./commit"
 export * from "./commitRecorder"
 export * from "./git"
 export * from "./TYPES"
+
+/**
+ * Default configuration
+ */
+localityAContainer.bind<Git>                 (BUGFINDER_LOCALITYRECORDER_COMMIT_TYPES.git).to(GitImpl)
+localityAContainer.bind<FormatParser>        (BUGFINDER_LOCALITYRECORDER_COMMIT_TYPES.gitCommitParser).to(FormatParser)
+localityAContainer.bind<MADFilesFromCommit>  (BUGFINDER_LOCALITYRECORDER_COMMIT_TYPES.madFilesFromCommitParser).to(MADFilesFromLogImpl)
